48b4f1f138c2f7e54b7cdda80dd88b843fb357ad
Alex-ITop <alex.toporkov.igorevich02@mail.ru>
2022-09-29 21:55:40 +0500

    Сортировка Пузырьком

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 
-N = int(input("Введите количество чисел (N): "))
+N = int(input("\nВведите количество чисел (N): "))
+dir = input("Введите направление сортировки\n0 - по убыванию, 1 - по возрастанию: ")
 a = []
 for i in range(N):
     a.append(randint(1, 99))
@@ -8,7 +9,12 @@ print(a)
 
 for i in range(N - 1):
     for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
-
+        if dir == '0':
+            if a[j] < a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+        elif dir == '1':
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+        else:
+            print("Некорректный ввод!")
 print(a)
